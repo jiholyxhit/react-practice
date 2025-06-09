32d78d7c33d739b0ed46d3d05a352a468af696d1
jihowoo <jihobbit8677@gmail.om>
2025-06-09 18:01:01 +0900

    Practice useEffect_mount&update

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
 import "./App.css";
-import {useRef} from "react"
+import {useState, useEffect} from "react"
 
 function App(){
-  const inputRef = useRef(null)
+  const [count1, setCount1] = useState(0)
+  const [count2, setCount2] = useState(0)
 
-  const handleFocus = () => {
-    console.log(inputRef)
-    console.log(inputRef.current)
-    inputRef.current.focus() 
-
+  function handleClick(setter){
+    
+    return setter(s => s+1)
   }
+
+  useEffect(() => {
+    console.log(`C1: ${count1}, C2: ${count2}`)
+  }, [count1])
 
   return(
     <div>
-      <input ref={inputRef}
-      type="text" placeholder="Type..." />
-      <button onClick={handleFocus}>
-        Focus Input
+      <h2>Count: {count1}</h2>
+      <button onClick={() => handleClick(setCount1)}>
+        Count1++
+      </button>
+
+      <h2>Count: {count2}</h2>
+      <button onClick={() => handleClick(setCount2)}>
+        Count2++
       </button>
     </div>
   )
